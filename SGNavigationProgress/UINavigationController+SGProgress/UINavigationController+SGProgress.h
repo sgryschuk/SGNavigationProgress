651c2bd45c3b5932a0bfc2503896f696672c56a3
Thijs Scheepers <tscheepers@users.noreply.github.com>
2014-05-17 10:38:06 +0200

    Update UINavigationController+SGProgress.h

--- SGNavigationProgress/UINavigationController+SGProgress/UINavigationController+SGProgress.h
+++ SGNavigationProgress/UINavigationController+SGProgress/UINavigationController+SGProgress.h
@@ -26,6 +26,11 @@
 - (void)setSGProgressPercentage:(float)percentage;
 - (void)setSGProgressPercentage:(float)percentage andTitle:(NSString *)title;
 - (void)setSGProgressPercentage:(float)percentage andTintColor:(UIColor *)tintColor;
+
+- (void)setSGProgressPercentage:(float)percentage duration:(float)duration;
+- (void)setSGProgressPercentage:(float)percentage duration:(float)duration andTitle:(NSString *)title;
+- (void)setSGProgressPercentage:(float)percentage duration:(float)duration andTintColor:(UIColor *)tintColor;
+
 - (void)setSGProgressMaskWithPercentage:(float)percentage;
 - (void)setSGProgressMaskWithPercentage:(float)percentage andTitle:(NSString *)title;
 
